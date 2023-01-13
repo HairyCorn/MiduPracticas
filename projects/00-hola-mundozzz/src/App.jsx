e96eba8b6c037d4000f021563a2058cfbb7ac033
HairyCorn <ariel.carrillo96@gmail.com>
2023-01-13 19:37:25 +0100

    First Commit

---
+++ projects/00-hola-mundozzz/src/App.jsx
@@ -0,0 +1,12 @@
+export function App() {
+return (
+    <article>
+        <header>
+            <img src="https://unavatar.io/midudev" alt="El avatar de Midu" />
+            <div>
+                <strong>My midudev</strong>
+            </div>
+        </header>
+    </article>
+)
+}
