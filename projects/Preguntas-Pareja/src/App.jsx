7350ffa6ff53291da71eeba602607891bac068fa
HairyCorn <ariel.carrillo96@gmail.com>
2023-01-31 22:21:41 +0100

    Add preguntas.js and update style

--- projects/Preguntas-Pareja/src/App.jsx
+++ projects/Preguntas-Pareja/src/App.jsx
@@ -1,13 +1,21 @@
-import "./App.css";
-import { TwitterFollowCard } from "./TwitterFollowCard";
+import preguntas from "./preguntas";
+import { useState, useEffect } from "react";
 export function App() {
+    const [preguntaActual, setPreguntaActual] = useState(0);
     return (
-        <section className="App">
-            <TwitterFollowCard isFollowing userName="midudev" name="Miguel Angel Durum" />
-            <TwitterFollowCard isFollowing={false} userName="7hells" name="Usuario Anonimo" />
-            <TwitterFollowCard isFollowing userName="socdimecres" name="Netflix Actriz" />
-            <TwitterFollowCard isFollowing userName="dramaqueen" name="La Reinazzz" />
-            <TwitterFollowCard isFollowing={false} userName="posello" name="El del LOL" />
-        </section>
+        <main className="app">
+            <div className="pregunta">
+                <div className="numero-pregunta">
+                    <span>Pregunta {preguntaActual + 1} de </span>{preguntas.length}
+                </div>
+                <div className="titulo-pregunta">
+                    {preguntas[preguntaActual].titulo}
+                </div>
+            </div>
+            <div className="respuesta">
+                <button>Respuesta 1</button>
+                <button>Respuesta 2</button>
+            </div>
+        </main>
     );
 }
